549686e001ef0553dc67400ab086585f9908cfcb
Student User <student@workstation.lab.example.com>
2022-12-06 17:03:43 -0500

    Changed index page contents.

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,4 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, World! Bonjour Bonjour >>  php version is " . PHP_VERSION . "\n";
+print "A change is a coming!\n";
 ?>
